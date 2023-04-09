b0ecf4962dc776b7ebe6ad5cdb63708f20f22462
YuvaraajE <59257764+YuvaraajE@users.noreply.github.com>
2023-04-09 20:23:35 +0530

    Added Message API and its tests

--- src/application/models.py
+++ src/application/models.py
@@ -1,4 +1,6 @@
 from application.db import db
+from sqlalchemy.sql import func
+
 
 class User(db.Model):
     __tablename__ = 'User'
@@ -8,32 +10,48 @@ class User(db.Model):
     display_name = db.Column(db.String, nullable=False)
     role = db.Column(db.Integer, nullable=False)
     
+    def __repr__(self):
+        return f'<User {self.username}>'
+    
 class Ticket(db.Model):
     __tablename__ = 'Ticket'
     ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     likes = db.Column(db.Integer, nullable=False, default=0)
-    created_date = db.Column(db.DateTime, nullable=False)
+    created_date = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     is_resolved = db.Column(db.String, nullable=False)
     subject = db.Column(db.String, nullable=False)
     catagory = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    
+    def __repr__(self):
+        return f'<Ticket {self.ticket_id}>'
 
 class Message(db.Model):
     __tablename__ = 'Message'
     message_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
-    created_date = db.Column(db.DateTime, nullable=False)
+    created_date =  db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     content = db.Column(db.String, nullable=False)
     ticket_id = db.Column(db.Integer, db.ForeignKey(Ticket.ticket_id))
 
+    def __repr__(self):
+        return f'<Message {self.message_id}>'
+    
 class Notification(db.Model):
     __tablename__ = 'Notification'
     notify_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     ticket_id = db.Column(db.Integer, db.ForeignKey(Ticket.ticket_id))
     message = db.Column(db.String, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    created_date =  db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
+
+    def __repr__(self):
+        return f'<Notification {self.notify_id}>'
 
 class Faq(db.Model):
     __tablename__ = 'Faq'
     ticket_id = db.Column(db.Integer, primary_key=True, autoincrement=True) 
     user_id = db.Column(db.Integer, db.ForeignKey(User.user_id))
+    
+    def __repr__(self):
+        return f'<FAQ {self.ticket_id}>'
