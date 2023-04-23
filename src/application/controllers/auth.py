6bd77239c5ef20278cffdabbe8f6546f9b8640bb
Hrithick Sundar J <125124965+Hrithick-Me@users.noreply.github.com>
2023-04-23 23:28:43 +0530

    Changed redirection to home endpoints

--- src/application/controllers/auth.py
+++ src/application/controllers/auth.py
@@ -38,7 +38,7 @@ def login():
         if user is not None and user.check_password(form.password.data):
             login_user(user)
             next = request.args.get("next")
-            return redirect(next or url_for('dashboard'))
+            return redirect(next or url_for('home'))
         flash('Invalid email address or Password.')    
     return render_template('login.html', form=form)
 
@@ -47,7 +47,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('home'))
+    return redirect(url_for('login'))
 
 
 @login_manager.unauthorized_handler
